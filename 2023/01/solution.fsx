8a70ba5bb67f1f420c77acdff98b3d199d140944
Aviral Goel <goel.aviral@gmail.com>
2023-12-03 17:13:31 -0800

    Format code

--- 2023/01/solution.fsx
+++ 2023/01/solution.fsx
@@ -32,10 +32,11 @@ let rec literalDigit (str: string) absent index next =
         literalDigit str absent (next index) next
 
 let anyDigit (str: string) literal finder comp =
-    let folder (index1, value1) (digitName:string, value2) =
+    let folder (index1, value1) (digitName: string, value2) =
         let index2 = finder str digitName
-        if comp index1 index2 then 
-            (index1, value1) 
+
+        if comp index1 index2 then
+            (index1, value1)
         else
             (index2, value2)
 
@@ -43,31 +44,28 @@ let anyDigit (str: string) literal finder comp =
 
 let literalLeft str = literalDigit str str.Length 0 inc
 
-let namedLeft str = anyDigit str literalLeft firstIndex (<) 
+let namedLeft str = anyDigit str literalLeft firstIndex (<)
 
 let literalRight str = literalDigit str 0 (str.Length - 1) dec
 
-let namedRight str = anyDigit str literalRight lastIndex (>) 
+let namedRight str = anyDigit str literalRight lastIndex (>)
 
-let makeNumber ((i1, v1), (i2, v2)) = v1 * 10 + v2 
+let makeNumber ((i1, v1), (i2, v2)) = v1 * 10 + v2
 
 let part1 input =
     input
-    |> List.map (fun str -> (literalLeft str, literalRight str)) 
+    |> List.map (fun str -> (literalLeft str, literalRight str))
     |> List.map makeNumber
     |> List.sum
 
 let part2 input =
     input
-    |> List.map (fun str -> (namedLeft str, namedRight str)) 
+    |> List.map (fun str -> (namedLeft str, namedRight str))
     |> List.map makeNumber
     |> List.sum
 
 let main filename =
-    let input = 
-        filename |>
-        System.IO.File.ReadLines |>
-        List.ofSeq
+    let input = filename |> System.IO.File.ReadLines |> List.ofSeq
 
     let solution1 = part1 input
     printfn $"Solution 1: {solution1}"
